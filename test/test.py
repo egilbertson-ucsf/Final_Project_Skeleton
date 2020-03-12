6539026e4a158345b19fcfdea46c4196e3d9853b
Erin Gilbertson <egilbertson@eg.15>
2020-03-12 16:12:30 -0700

    'test'

--- test/test.py
+++ test/test.py
@@ -1,9 +1,11 @@
 from scripts import NN
 
+## See jupyter notebook 'WriteUp.ipynb' for testing
 
 def test_encoder():
-
+    return
 def test_encoder_relu():
-
+    return
 
 def test_one_d_ouput():
+    return
